9f1e498eb4da960ffdd4e264ad957e3a7935f31f
HyojinPark1223 <gywls9712@naver.com>
2025-04-17 23:20:23 +0900

    250417 study

--- 2025/250417_Baekjoon1766.py
+++ 2025/250417_Baekjoon1766.py
@@ -1,5 +1,36 @@
 # 25.04.17 우선순위 큐
-# 처음에 조합으로 풀었는데 시간 초과났다. 시간 복잡도가 2초라서 NlogN의 시간복잡도 알고리즘을 사용해야 했던 것.
+# 처음 보는 문제였다.. 우선순위 큐라는 게 있다는 걸 첨 알았음.
+# 앞 번호부터 체크해본 뒤에, 연결 되어있는 노선이 있다면(먼저 계산해야하는 것이 있다면) 넣어주지 않음.
+# 연결된 문제(예를들면 3, 1)의 경우 앞을 우선순위 큐로 queue의 앞으로 연결된 걸 땡겨 넣어준다.
 
 import sys
 sys.stdin = open("input.txt", 'r')
+
+import heapq
+
+n, m = map(int, input().split())
+
+answer = []
+graph = [[] for _ in range(n + 1)]
+inDegree = [0 for _ in range(n+1)]
+queue = []
+
+for i in range(m):
+    first, second = map(int, input().split())
+    graph[first].append(second)
+    inDegree[second] += 1
+
+for i in range(1, n + 1):
+    if inDegree[i] == 0:
+        heapq.heappush(queue, i)
+
+while queue:
+    tmp = heapq.heappop(queue)
+    answer.append(tmp)
+    for i in graph[tmp]:
+        inDegree[i] -= 1
+        if inDegree[i] == 0:
+            heapq.heappush(queue, i)
+
+
+print(" ".join(map(str, answer)))
--- 2025/250417_Baekjoon2143.py
+++ 2025/250417_Baekjoon2143.py
@@ -1,5 +1,39 @@
 # 25.04.17 누적합
-# 처음에 조합으로 풀었는데 시간 초과났다. 시간 복잡도가 2초라서 NlogN의 시간복잡도 알고리즘을 사용해야 했던 것.
+# 누적합은 아무리 봐도 이해가 안된다.. 다시 공부해야할듯.
 
 import sys
 sys.stdin = open("input.txt", 'r')
+
+import bisect
+
+T = int(input())
+a = int(input())
+an = list(map(int, input().split()))
+b = int(input())
+bn = list(map(int, input().split()))
+
+aan, bbn = [], []   # an, bn의 누적합을 저장할 리스트
+for i in range(a):
+    for j in range(i + 1, a + 1):
+        aan.append(sum(an[i:j]))
+for i in range(b):
+    for j in range(i + 1, b + 1):
+        bbn.append(sum(bn[i:j]))
+
+aan.sort()
+bbn.sort()
+
+# aan의 원소를 하나씩 체크하면서 합이 T가 되는 bbn의 원소 이진탐색으로 검색(탐색을 위해 위에서 소트해줌)
+# 혹은 누적합을 구하면서 T를 사용해서 이미 계산 하여 구하는 방법.
+ans = 0
+for i in range(len(aan)):
+    bmp = T - aan[i]
+
+    # bisect 라이브러리를 사용해서 bbn 리스트에 bmp 값이 존재하는지 index 값을 알 수가 있다.
+    left = bisect.bisect_left(bbn, bmp)
+    right = bisect.bisect_right(bbn, bmp)
+    ans += (right - left)
+
+print(ans)
+
+#참고 https://jaehwaseo.tistory.com/49
